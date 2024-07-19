08b206ebfba9076465da7916750830e30b57a39e
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-07-19 11:20:57 -0300

    feat: update types and swagger doc title

--- dist/config/swagger.config.js
+++ dist/config/swagger.config.js
@@ -9,7 +9,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: ' GF challenge - Digimons API - Swagger documentation',
+            title: '🚀 GF challenge - Digimons API - Swagger documentation.',
             version: '1.0.0',
         },
     },
--- src/repositories/digimon.repository.ts
+++ src/repositories/digimon.repository.ts
@@ -7,8 +7,8 @@ export interface IDigimonRepository {
   findAll(data?: any): Promise<WithId<Digimon>[] | undefined>;
   findByName(name: string): Promise<WithId<Digimon>[] | undefined>;
   findByLevel(level: string): Promise<WithId<Digimon>[] | undefined>;
-  insertOne(data?: any): Promise<ObjectId | undefined>;
-  updateOne(id: string, data?: any): Promise<number | undefined>;
+  insertOne(data: Omit<Digimon, 'id'>): Promise<ObjectId | undefined>;
+  updateOne(id: string, data?: Digimon): Promise<number | undefined>;
   deleteOne(id: string): Promise<number | undefined>;
 }
 
@@ -18,7 +18,7 @@ export class DigimonRepository implements IDigimonRepository {
   findById(id: string): Promise<ObjectId | undefined> {
     return this.dbStrategy.findById(this.collectionName, id);
   }
-  updateOne(id: string, data?: any): Promise<number | undefined> {
+  updateOne(id: string, data?: Digimon): Promise<number | undefined> {
     return this.dbStrategy.updateOne(this.collectionName, id, data);
   }
   deleteOne(id: string): Promise<number | undefined> {
@@ -33,7 +33,7 @@ export class DigimonRepository implements IDigimonRepository {
   findByLevel(level: string): Promise<WithId<Digimon>[] | undefined> {
     return this.dbStrategy.findByLevel(this.collectionName, level);
   }
-  insertOne(data?: any): Promise<ObjectId | undefined> {
+  insertOne(data: Omit<Digimon, 'id'>): Promise<ObjectId | undefined> {
     return this.dbStrategy.insertOne(this.collectionName, data);
   }
 }
